a8320e57e7488d5623462552110d8f0d7216546b
reshad21 <miakala7@gmail.com>
2026-02-01 13:19:31 +0600

    feat: Add global styles, layout, and shared components
    - Introduced global CSS styles including typography, utilities, and themes.
    - Created a RootLayout component to wrap the application with necessary providers and styles.
    - Implemented shared components: Footer, Navber, ScrollToTop, and Topber for consistent UI.
    - Added custom hooks for role management and mobile detection.
    - Developed API request utilities for handling authentication and data fetching.
    - Implemented role management services for CRUD operations.
    - Created types for authentication, roles, and department heads.
    - Added utility functions for debounce and HTML rendering.

---
+++ src/app/(common)/layout.tsx
@@ -0,0 +1,17 @@
+
+import Footer from "@/src/components/shared/Footer";
+import Navber from "@/src/components/shared/Navber";
+import Topber from "@/src/components/shared/Topber";
+
+const CommonLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Topber />
+      <Navber />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default CommonLayout;
---
+++ src/components/shared/Navber.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navber = () => {
+  return (
+    <div>
+      this is navber
+    </div>
+  )
+}
+
+export default Navber
