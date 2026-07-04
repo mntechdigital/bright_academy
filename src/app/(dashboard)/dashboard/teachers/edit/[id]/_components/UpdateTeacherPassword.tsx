09df898f0ed60d3d21dc8caae3c24722e8c2d430
reshad21 <miakala7@gmail.com>
2026-07-04 13:13:24 +0600

    pull from main

--- src/app/(dashboard)/dashboard/teachers/edit/[id]/_components/UpdateTeacherPassword.tsx
+++ src/app/(dashboard)/dashboard/teachers/edit/[id]/_components/UpdateTeacherPassword.tsx
@@ -40,7 +40,7 @@ export default function UpdateTeacherPassword({ teacher }: Props) {
   } = useForm<UpdatePasswordFormValues>({
     mode: "onBlur",
     defaultValues: {
-      currentPassword: teacher.password || "",
+      currentPassword: "",
       newPassword: "",
       confirmPassword: "",
     },
